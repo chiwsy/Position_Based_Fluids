3943e1a160e6ad1cdc332e2729890eeae2760df6
chiwsy <chiwsylove@gmail.com>
2014-11-25 23:34:59 -0500

    Revert "fix visualization bugs"
    This reverts commit e91bc544ee1d66b24007f43eb105f0c22f9f2f28.

--- PBF_Stage2/Ref/PROJ_WIN/Project3/shaders/planetGS.glsl
+++ PBF_Stage2/Ref/PROJ_WIN/Project3/shaders/planetGS.glsl
@@ -1,7 +1,6 @@
 #version 150
 
 uniform mat4 u_projMatrix;
-uniform mat4 u_viewMatrix;
 uniform vec3 u_cameraPos;
 
 layout (points) in;
@@ -23,11 +22,9 @@ void main()
     WorldCoord = Position;
 	
 
-	ToCam =normalize(u_cameraPos - Position);
-	Up = vec3(0.0, 0.0, 1.0);
-	//if (dot(ToCam,Up)>.9999)
-
-	Right = normalize(cross(Up,ToCam));
+    ToCam = normalize(u_cameraPos - Position);
+    Up = vec3(0.0, 0.0, 1.0);
+    Right = cross(ToCam, Up);
     Up = cross(Right, ToCam);
 
     vec3 Pos = Position + scale*Right - scale*Up;
